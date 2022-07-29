fbf474063d3364f13787b6bb0fcc516ad99815c9
Thomas Le <tle1321@gmail.com>
2022-07-29 18:19:19 -0500

    save

---
+++ script.js
@@ -0,0 +1,39 @@
+const player1 = {
+    maxHp: 100,
+    hp: 100,
+    attack: 10,
+    accuracy: 10,
+    gold: 20,
+    name:  '',
+}
+
+const enemies = [
+    {
+        name: "monster-1",
+        maxHp: 50,
+        hp: 50,
+        attack: 10,
+        accuracy: 5,
+        gold: 15,
+    },
+    {
+        name: "monster-2",
+        maxHp: 70,
+        hp: 70,
+        attack: 15,
+        accuracy: 5,
+        gold: 35,
+    },
+    {
+        name: "monster-3",
+        maxHp: 100,
+        hp: 100,
+        attack: 35,
+        accuracy: 3,
+        gold: 100,
+    }
+]
+
+
+alert("welcome to the game")
+player1.name = 
