ebc1a3c447967b9f70d4335f057224d6ff2c549b
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-08 14:11:45 -0500

    link to the function that is either going to display the icon or display a weather image in the background

--- weatherAPI.js
+++ weatherAPI.js
@@ -27,21 +27,27 @@ function getCoordinates(city) {
 }
 
 
-
 //Weather API 
 function getCurrentWeather(lat, lon) {
     let requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&units=imperial&appid=9fa809658341d19670907599fff8fcdc';
-
+    
     fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
+    .then(function (response) {
+        return response.json();
     })
     .then(function (data) {
-        console.log(data.current.weather[0].main);
+        // console.log(data.current.weather[0].main);
+        let currentWeatherCondition = data.current.weather[0].main;
+        displayClimate(currentWeatherCondition);
     });
 }
 
 const weatherConditions = ['Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Atmosphere', 'Clear', 'Clouds'];
+
+function displayClimate (currentWeatherCondition) {
+    console.log(currentWeatherCondition);
+}
+
 //display weather icon
 // function displayingCurrentIcon (){
 //     document.getElementById('current-weather-icon').src="https://openweathermap.org/img/w/"+ currentIcon +".png"; 
